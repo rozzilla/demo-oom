b325004e129c9d947604e62645555fa8f374c4d5
Roberto Bianchi <roberto.bianchi@spendesk.com>
2024-09-12 13:04:30 +0200

    Update list endpoint

--- src/routes/list.js
+++ src/routes/list.js
@@ -1,10 +1,10 @@
-import { addToList } from "../utils.js";
+import { totalList } from "../utils.js";
 
 export default async function (fastify) {
   fastify.get("/list", async (request) => {
     const { size } = request.query;
     const arraySize = parseInt(size) || 1;
-    const result = addToList(arraySize);
+    const result = totalList(arraySize);
 
     return { result };
   });
--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,10 @@
 const list = [];
+// let total = 0;
 
-export function addToList(size) {
+export function totalList(size) {
   for (let i = 0; i < size; i++) {
     list.push("Value " + i);
   }
   return list.length;
+  // return (total += size);
 }
